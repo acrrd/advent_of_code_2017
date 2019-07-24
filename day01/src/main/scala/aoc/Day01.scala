97c96843bf51b6967ce58d3db7f007caea588aef
Andrea Corradi <andreac@unstable.it>
2019-07-24 20:45:34 +0200

    [day01] Add solution for day 01 part 2

--- day01/src/main/scala/aoc/Day01.scala
+++ day01/src/main/scala/aoc/Day01.scala
@@ -7,19 +7,30 @@ object Day01 {
     val captcha = Source.stdin.getLines.mkString
 
     println(Part1.solve(captcha))
+    println(Part2.solve(captcha))
   }
 
   def parseCaptcha(captcha: String): Seq[Int] = {
     captcha.map(_.asDigit)
   }
 
+  def solve_common(captcha: String)(genSecondList: Seq[Int] => Seq[Int]): Int = {
+    val digits = parseCaptcha(captcha)
+    digits.zip(genSecondList(digits)).filter({ case (a, b) => a == b }).foldLeft(0)((acc, p) => acc + p._1)
+  }
+
   object Part1 {
     def solve(captcha: String): Int = {
-      solve(parseCaptcha(captcha))
+      solve_common(captcha)(digits => digits.tail ++ List(digits(0)))
     }
+  }
 
-    def solve(digits: Seq[Int]): Int = {
-      digits.zip(digits.tail ++ List(digits(0))).filter({ case (a, b) => a == b }).foldLeft(0)((acc, p) => acc + p._1)
+  object Part2 {
+    def solve(captcha: String): Int = {
+      solve_common(captcha){ digits =>
+        val middle = digits.length/2
+        digits.drop(middle) ++ digits.take(middle)
+      }
     }
   }
 }
--- day01/src/test/scala/aoc/Day01Spec.scala
+++ day01/src/test/scala/aoc/Day01Spec.scala
@@ -18,4 +18,24 @@ class Day01Spec extends FlatSpec with Matchers {
   "Day01.Part1" should "solve captcha 91212129" in {
     Day01.Part1.solve("91212129") shouldEqual 9
   }
+
+  "Day01.Part2" should "solve captcha 1212" in {
+    Day01.Part2.solve("1212") shouldEqual 6
+  }
+
+  "Day01.Part2" should "solve captcha 1221" in {
+    Day01.Part2.solve("1221") shouldEqual 0
+  }
+
+  "Day01.Part2" should "solve captcha 123425" in {
+    Day01.Part2.solve("123425") shouldEqual 4
+  }
+
+  "Day01.Part2" should "solve captcha 123123" in {
+    Day01.Part2.solve("123123") shouldEqual 12
+  }
+
+  "Day01.Part2" should "solve captcha 12131415" in {
+    Day01.Part2.solve("12131415") shouldEqual 4
+  }
 }
