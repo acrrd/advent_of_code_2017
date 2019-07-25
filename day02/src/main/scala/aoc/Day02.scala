aa72d95642607b2e858174fbb26f2d8115aeacd9
Andrea Corradi <andreac@unstable.it>
2019-07-25 23:14:05 +0200

    [day02] Add solution for day 02 part 2

--- day02/src/main/scala/aoc/Day02.scala
+++ day02/src/main/scala/aoc/Day02.scala
@@ -5,21 +5,45 @@ import scala.io.Source
 object Day02 {
   def main(args: Array[String]): Unit = {
     val spreadsheet = Source.stdin.mkString
-    
-    println(Part1.checksum(spreadsheet))
-  }
 
+    def checksumWith: (Seq[Int] => Int) => Int = checksum_common(spreadsheet)
+
+    println(checksumWith(Part1.checksum_row))
+    println(checksumWith(Part2.checksum_row))
+  }
 
   def parseSpreadsheet(spreadsheet: String): Iterator[Seq[Int]] = {
     augmentString(spreadsheet).lines.map(_.trim.split("\\s+").map(_.toInt))
   }
 
+  def checksum_common(spreadsheet: String)(getN: Seq[Int] => Int) : Int = {
+    parseSpreadsheet(spreadsheet).map(getN).sum
+  }
+
   object Part1 {
+    def checksum_row(row: Seq[Int]): Int = {
+      row.max - row.min
+    }
+
     def checksum(spreadsheet: String): Int = {
-      parseSpreadsheet(spreadsheet).map(vs => {
-        vs.max - vs.min
-      }).sum
+      checksum_common(spreadsheet)(Part1.checksum_row)
     }
   }
 
+  object Part2 {
+    def checksum_row(row: Seq[Int]): Int = {
+      def ns = for (
+        (a, i) <- row.zipWithIndex;
+        b <- row.drop(i + 1)
+      ) yield if(a > b) (a, b) else (b, a)
+
+      val (a, b) = ns.find{ case (a, b) => a % b == 0} .get
+
+      a / b
+    }
+
+    def checksum(spreadsheet: String): Int = {
+      checksum_common(spreadsheet)(Part2.checksum_row)
+    }
+  }
 }
--- day02/src/test/scala/aoc/Day02Spec.scala
+++ day02/src/test/scala/aoc/Day02Spec.scala
@@ -6,4 +6,8 @@ class Day02Spec extends FlatSpec with Matchers {
   "Day02.Part1" should "checksum" in {
     Day02.Part1.checksum("5 1  9 5\n7   5  3\n2   4   6 8") shouldEqual 18
   }
+
+  "Day02.Part2" should "checksum" in {
+    Day02.Part2.checksum("5 9 2 8\n9 4 7 3\n3 8 6 5") shouldEqual 9
+  }
 }
