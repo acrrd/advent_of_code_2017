8c580795f58c587b928e19e6e9f1d1373d6ca85d
Andrea Corradi <andreac@unstable.it>
2019-08-25 17:13:52 +0200

    [day04] Add solution for day 4 part 2

--- day04/src/main/scala/aoc/Day04.scala
+++ day04/src/main/scala/aoc/Day04.scala
@@ -6,8 +6,18 @@ object Day04 {
   def main(args: Array[String]): Unit = {
     val passwords = Source.stdin.getLines().toSeq
 
-    val count = Part1.countValidPasswords(passwords)
-    println(count)
+    println(Part1.countValidPasswords(passwords))
+    println(Part2.countValidPasswords(passwords))
+  }
+
+  def isPasswordValidCommon(password: String, f: String => String) = {
+    val words = password.split(" ")
+    val uniqueWords = words.foldLeft(Set[String]())( (uwords: Set[String], word: String) => {
+      val sw = f(word)
+      uwords + sw
+    })
+
+    words.length == uniqueWords.size
   }
 
   object Part1 {
@@ -15,13 +25,14 @@ object Day04 {
       passwords.filter(isPasswordValid).length
     }
 
-    def isPasswordValid(password: String) = {
-      val words = password.split(" ")
-      val uniqueWords = words.foldLeft(Set[String]())( (uwords: Set[String], word: String) => {
-        uwords + word
-      })
+    def isPasswordValid(password: String) = isPasswordValidCommon(password, w => w)
+  }
 
-      words.length == uniqueWords.size
+  object Part2 {
+    def countValidPasswords(passwords: Seq[String]) = {
+      passwords.filter(isPasswordValid).length
     }
+
+    def isPasswordValid(password: String) = isPasswordValidCommon(password, w => w.toCharArray.sorted.mkString)
   }
 }
--- day04/src/test/scala/aoc/Day04Spec.scala
+++ day04/src/test/scala/aoc/Day04Spec.scala
@@ -7,11 +7,31 @@ class Day04Spec extends FlatSpec with Matchers {
     Day04.Part1.isPasswordValid("aa bb cc dd ee") shouldEqual true
   }
 
-  "Day04 Part 1" should "pwd 'aa bb cc dd aa' is valid" in {
+  "Day04 Part 1" should "pwd 'aa bb cc dd aa' is not valid" in {
     Day04.Part1.isPasswordValid("aa bb cc dd aa") shouldEqual false
   }
 
   "Day04 Part 1" should "pwd 'aa bb cc dd aaa' is valid" in {
     Day04.Part1.isPasswordValid("aa bb cc dd aaa") shouldEqual true
+  }
+
+  "Day04 Part 2" should "pwd 'abcde fghij' is valid" in {
+    Day04.Part2.isPasswordValid("abcde fghij") shouldEqual true
+  }
+
+  "Day04 Part 2" should "pwd 'abcde xyz ecdab' is not valid" in {
+    Day04.Part2.isPasswordValid("abcde xyz ecdab") shouldEqual false
+  }
+
+  "Day04 Part 2" should "pwd 'a ab abc abd abf abj' is valid" in {
+    Day04.Part2.isPasswordValid("a ab abc abd abf abj") shouldEqual true
+  }
+
+  "Day04 Part 2" should "pwd 'iiii oiii ooii oooi oooo' is valid" in {
+    Day04.Part2.isPasswordValid("iiii oiii ooii oooi oooo") shouldEqual true
+  }
+
+  "Day04 Part 2" should "pwd 'oiii ioii iioi iiio' is not valid" in {
+    Day04.Part2.isPasswordValid("oiii ioii iioi iiio") shouldEqual false
   }
 }
