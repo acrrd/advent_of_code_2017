f6b1f09f3d7a46052acf467ba0e3c6215f869945
Andrea Corradi <andreac@unstable.it>
2019-09-03 21:35:35 +0200

    [day05] Add solution for day 05 part 2

--- day05/src/main/scala/aoc/Day05.scala
+++ day05/src/main/scala/aoc/Day05.scala
@@ -6,28 +6,41 @@ import scala.io.Source
 object Day05 {
   def main(args: Array[String]): Unit = {
     val offsets = Source.stdin.getLines().toSeq.map(_.toInt)
-    
+
     println(Part1.countStepsToExit(offsets))
+    println(Part2.countStepsToExit(offsets))
+  }
+
+  def countStepsToExitCommon(offsets: Seq[Int], newValue: Int => Int): Int = {
+    var os = MSeq[Int]()
+    os ++= offsets
+    var steps = 0
+    var index = 0
+
+
+    while (index >= 0 && index < os.length) {
+
+      steps += 1
+      val oldIndex = index
+      val offset = os(index)
+      index += offset
+      os(oldIndex) += newValue(offset)
+    }
+
+    steps
   }
 
   object Part1 {
-    def countStepsToExit(offsets: Seq[Int]): Int = {
-      var os = MSeq[Int]()
-      os ++= offsets
-      var steps = 0
-      var index = 0
+    def countStepsToExit(offsets: Seq[Int]): Int = countStepsToExitCommon(offsets, _ => 1)
+  }
 
+  object Part2 {
+    def countStepsToExit(offsets: Seq[Int]): Int = countStepsToExitCommon(offsets, offset => if (offset >= 3) {
+      -1
+    } else {
+      1
+    })
 
-      while (index >= 0 && index < os.length) {
-
-        steps += 1
-        val oldIndex = index
-        index += os(index)
-        os(oldIndex) += 1
-      }
-
-      steps
-    }
   }
 
 }
--- day05/src/test/scala/aoc/Day05Spec.scala
+++ day05/src/test/scala/aoc/Day05Spec.scala
@@ -3,7 +3,11 @@ package aoc
 import org.scalatest._
 
 class Day05Spec extends FlatSpec with Matchers {
-  "Day05 Part 1" should "exit in 3 steps with [0, 3,  0,  1, -3]" in {
+  "Day05 Part 1" should "exit in 5 steps with [0, 3,  0,  1, -3]" in {
     Day05.Part1.countStepsToExit(List(0, 3,  0,  1, -3)) shouldEqual 5
+  }
+
+  "Day05 Part 2" should "exit in 10 steps with [0, 3,  0,  1, -3]" in {
+    Day05.Part2.countStepsToExit(List(0, 3,  0,  1, -3)) shouldEqual 10
   }
 }
